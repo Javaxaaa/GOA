f40e08e329033eda2b8b4c07fe42b61fbe1991b7
Javaxaaa <ltsjavakhishvili03@gmail.com>
2024-05-11 22:29:30 +0400

    commit

---
+++ Day 01/homework/homework.py
@@ -0,0 +1,7 @@
+name = "Sandro"
+surname = "Javakhishvili"
+height = "1,70"
+story = "გოაში შემოვედი, რადგან მსურს ვისწავლო პროგრამირებე მაღალ დონემდე და ვისწავლო web development"
+age = "14"
+
+print(name + " " + surname + ", " + height + ", " + story + ", " + age)
