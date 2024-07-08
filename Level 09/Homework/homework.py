525a88122e84b2aaa0bf6bba9fa851a227d757f5
Javaxaaa <ltsjavakhishvili03@gmail.com>
2024-07-08 13:51:48 +0400

    commit

---
+++ Level 09/Homework/homework.py
@@ -0,0 +1,66 @@
+# print(10 > 5)
+# print(6 > 2)
+# print(8 > 4)
+# print(2 > 1)
+# print(7 > 3)
+# print(7 < 10)
+# print(1 < 4)
+# print(7 < 9)
+# print(5 < 6)
+# print(2 < 4)
+# print(10 >= 10)
+# print(25 >= 4)
+# print(4 >= 1)
+# print(21 >= 21)
+# print(12 >= 3)
+# print(10 <= 10)
+# print(3 <= 21)
+# print(5 <= 16)
+# print(21 <= 21)
+# print(5 <= 6)
+# print(100 == 100)
+# print(112 == 112)
+# print(15 == 12)
+# print(24 == 51)
+# print(1 == 1)
+# print(1 != 121)
+# print(5 != 115)
+# print(2 != 72)
+# print(61 != 55)
+# print(111111211 != 1)
+
+
+
+
+# user1 = int(input("Enter first age: "))
+# user2 = int(input("Enter second age: "))
+
+
+# if user1 > user2:
+#     print("user1 is bigger than user2. ")
+
+# elif user1 == user2:
+#     print("The age is the same!")
+
+# else:
+#     print("user2 is bigger than user1. ")
+
+
+
+
+
+# savings = int(input("Enter savings: "))
+
+# savings_after_1year = savings + savings * 5 / 100
+
+# print(savings_after_1year)
+
+
+
+# score = int(input("Enter your score: "))
+
+# if score >= 100:
+#     print(" You moved to the next level! ")
+
+# else:
+#     print(" You need more than 100 score to level up. ")
