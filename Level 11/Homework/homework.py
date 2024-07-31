004f6bba6572adc88d24ee089148b26e389b2de9
Javaxaaa <ltsjavakhishvili03@gmail.com>
2024-07-31 22:31:43 +0400

    commit

---
+++ Level 11/Homework/homework.py
@@ -0,0 +1,45 @@
+# name=input("Enter your name: ")
+# my_name="Sandro"
+
+
+# if name == my_name:
+#     print(True)
+
+
+# else:
+#     print(False)
+
+
+# number=int(input)("Enter your number")
+
+
+# if number < 10 or number > 0:
+#     print(True)
+
+# else:
+#     print(False)
+
+
+
+
+# 3 > 2
+# 15 > 4
+# 12 < 41
+# 71 > 12
+# 12 > 4
+# 15 < 161
+# 15 > 11
+# 651 > 3
+# 3 > 1
+# 90 < 900
+# 15 > 1
+# 99 > 1
+# 100 < 155
+# 981 > 512
+# 1633 < 12312
+# 872 < 1234
+# 8721 > 124
+# 5 > 1
+# 8 < 11
+# 1 < 10
+# 25 > 11
