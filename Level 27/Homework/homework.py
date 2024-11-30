ea3c3913ea856a47eb00c11eebc919845233a5bf
Javaxaaa <ltsjavakhishvili03@gmail.com>
2024-11-30 14:30:52 +0400

    commit

---
+++ Level 27/Homework/homework.py
@@ -0,0 +1,37 @@
+# def greet(name, place):
+#     return f"Hello {name}, welcome to {place}"
+# print(greet("Alice", "Wonderland"))
+
+#############################################
+
+# words = ["apple", "banana", "cherry"]
+# fruit_string = ", ".join(words)
+# print(fruit_string)
+
+#############################################
+
+
+# sentence = "The quick brown fox jumps over the lazy dog."
+# words_list = sentence.split()
+# print(words_list)
+
+
+#############################################
+
+# quote = "To be or not to be, that is the question."
+# modified_quote = quote.replace("be", "code")
+# print(modified_quote)
+
+#############################################
+
+# mixed_case = "PyThOn Is AwEsOmE!"
+# lowercase_string = mixed_case.lower()
+# print(lowercase_string)
+
+#############################################
+
+# greeting = "good morning"
+# uppercase_greeting = greeting.upper()
+# print(uppercase_greeting)
+
+#############################################
